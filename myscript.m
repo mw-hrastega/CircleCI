d0467bd73656edcec33e2c874d8073d727c43284
mw-hrastega <48831250+mw-hrastega@users.noreply.github.com>
2020-09-02 14:36:10 -0400

    Update myscript.m

--- myscript.m
+++ myscript.m
@@ -1,6 +1,15 @@
 disp(matlabroot)
-% Run all unit tests in my repository.
-results = runtests('IncludeSubfolders', true);
-
-% Assert no tests failed.
-assert(all(~[results.Failed]));
+import matlab.unittest.TestRunner
+import matlab.unittest.Verbosity
+import matlab.unittest.plugins.CodeCoveragePlugin
+import matlab.unittest.plugins.codecoverage.CoberturaFormat
+ 
+suite = testsuite(pwd, 'IncludeSubfolders', true);
+ 
+runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
+runner.addPlugin(CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true, 'Producing', CoberturaFormat('coverage.xml')));
+ 
+results = runner.run(suite);
+ 
+nfailed = nnz([results.Failed]);
+assert(nfailed == 0, [num2str(nfailed) ' test(s) failed.']);
