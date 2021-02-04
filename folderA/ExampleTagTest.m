06602b49faa238d47a19a717feded001f38be383
mw-hrastega <48831250+mw-hrastega@users.noreply.github.com>
2021-02-04 16:44:29 -0500

    Update ExampleTagTest.m

--- folderA/ExampleTagTest.m
+++ folderA/ExampleTagTest.m
@@ -2,24 +2,29 @@ classdef ExampleTagTest < matlab.unittest.TestCase
     methods (Test)
         function testA (testCase)
             % test code
+            disp('a')
         end
     end
     methods (Test, TestTags = {'Unit'})
         function testB (testCase)
             testCase.assertTrue(false)
+            disp('b')
         end
         function testC (testCase)
             % test code
+            disp('c')
         end
     end
     methods (Test, TestTags = {'Unit','FeatureA'})
         function testD (testCase)
             % test code
+            disp('d')
         end
     end
     methods (Test, TestTags = {'System','FeatureA'})
         function testE (testCase)
             % test code
+            disp('e')
         end
     end
 end
